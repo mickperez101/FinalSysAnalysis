bcb5aab4c2449614669764df6aadb4920d991271
mickperez101 <mickperez101@gmail.com>
2022-04-21 17:15:39 -0700

    Final Project

--- flask_app/db_test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-conn = sql.connect("database.db")
-
-conn.execute("CREATE TABLE students (name TEXT, address TEXT, city TEXT, pin TEXT)")
-
-conn.close()
-
-print("Table created successfully")
